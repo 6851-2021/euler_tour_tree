19995404e0ba0e6c00e0ca732e425d1dc81bed7e
Isabella Kang <ikang@mit.edu>
2021-05-19 16:45:37 -0400

    Attempt to add LaTeX to matplotlib.

--- plot_benchmark.py
+++ plot_benchmark.py
@@ -1,13 +1,36 @@
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+import scipy.stats
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+
+
 
 
 with open('out.out', 'rb') as f:
     read = pickle.load(f)
 
 ns, runtimes, stdevs = read
+runtimes = np.array(runtimes)/10
 print(read)
 
-plt.errorbar(np.log(ns), runtimes, stdevs)
+x = np.log(ns)
+m,b = np.polyfit(x, runtimes, 1)
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, runtimes)
+print("r**2 is", r_value**2)
+print('slope and intercept are', m, b)
+diff_squared = []
+
+for i, elt in enumerate(x):
+    diff_squared.append((runtimes[i]-(m*elt + b))**2/(stdevs[i]/np.sqrt(10)/10)**2)
+print("chi squared is", sum(diff_squared))
+
+
+plt.errorbar(x, runtimes, [s/np.sqrt(10)/10 for s in stdevs], fmt="b.")
+plt.plot(x, m*x+b, 'r')
+plt.title("Log(n) vs average runtime per operation on size n ETT")
+plt.xlabel('Log(n)')
+plt.ylabel('Runtimes ($\mu$s)')
 plt.show()
